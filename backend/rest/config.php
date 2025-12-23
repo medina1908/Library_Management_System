c05ee7af2fa8c7f3f4ac615fdad3a0291f3e8d25
medina1908 <medina.alic@stu.ibu.edu.ba>
2025-12-23 21:08:04 +0100

    changes in backend folder

--- backend/rest/config.php
+++ backend/rest/config.php
@@ -1,37 +1,24 @@
 <?php
-// Set the reporting
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL ^ (E_NOTICE | E_DEPRECATED));
-
-
-class Config
-{
-   public static function DB_NAME()
-   {
-       return 'library_management_system'; 
+class Config {
+   public static function DB_NAME() {
+       return Config::get_env("DB_NAME", "library_management_system");
    }
-   public static function DB_PORT()
-   {
-       return  3306;
+   public static function DB_PORT() {
+       return Config::get_env("DB_PORT", 3306);
    }
-   public static function DB_USER()
-   {
-       return 'root';
+   public static function DB_USER() {
+       return Config::get_env("DB_USER", 'root');
    }
-   public static function DB_PASSWORD()
-   {
-       return '';
+   public static function DB_PASSWORD() {
+       return Config::get_env("DB_PASSWORD", '');
    }
-   public static function DB_HOST()
-   {
-       return '127.0.0.1';
+   public static function DB_HOST() {
+       return Config::get_env("DB_HOST", '127.0.0.1');
    }
-
-
-public static function JWT_SECRET() {
-    return 'a-string-secret-at-least-256-bits-long';
+   public static function JWT_SECRET() {
+       return Config::get_env("JWT_SECRET", 'a-string-secret-at-least-256-bits-long');
+   }
+   public static function get_env($name, $default){
+       return isset($_ENV[$name]) && trim($_ENV[$name]) != "" ? $_ENV[$name] : $default;
+   }
 }
-
-}
-
